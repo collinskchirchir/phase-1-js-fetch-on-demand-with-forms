8a09a3a9f5ffeab3a09c778f88e5ccc71c827267
Collins Chirchir <collinskchirchir@gmail.com>
2022-12-06 23:38:45 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,4 +1,25 @@
+function renderData(data)
+{
+  const title = document.querySelector('section#movieDetails h4');
+  title.textContent = data.title;
+  const summary = document.querySelector('section#movieDetails p');
+  summary.textContent = data.summary;
+}
+
 const init = () => {
+  const inputForm = document.querySelector('form');
+  inputForm.addEventListener('submit',(e) => {
+    e.preventDefault();
+    // console.log(e)
+    const input = e.target.querySelector('#searchByID').value;
+    console.log(input);
+
+    fetch(`http://localhost:3000/movies/${input}`)
+    .then(res => res.json())
+    .then(data => renderData(data))
+
+  })
+
   
 }
 
